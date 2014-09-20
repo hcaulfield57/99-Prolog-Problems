226a07a1c5062937f654404d8e589189cb8ee7a1
Grant Mather <hcaulfield57@gmail.com>
2014-09-19 19:42:10 -0600

    two more

--- pb5.pl
+++ pb5.pl
@@ -1,4 +1,7 @@
 % Problem 5
 % Reverse a list.
 
+reverse_list(X, Y) :- reverse_list(X, [], Y).
 
+reverse_list([], A, A).
+reverse_list([H|T], A, R) :- reverse_list(T, [H|A], R).
---
+++ pb6.pl
@@ -0,0 +1,4 @@
+% Problem 6
+% Find out whether a list is a palindrome
+
+is_palindrome(X) :- reverse(X, X).
